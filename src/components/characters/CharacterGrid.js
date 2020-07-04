740c1112c8f2de68d59bddfdb8f2049ffdf3a373
caren1 <czarnocki.w@wp.pl>
2020-07-04 15:32:39 +0200

    constructed JSX for characterItem

--- src/components/characters/CharacterGrid.js
+++ src/components/characters/CharacterGrid.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import CharacterItem from './CharacterItem'
 
 const CharacterGrid = ( { isLoading, items } ) => {
     return isLoading ? (<h1>Loading...</h1>) : (<section className="cards">
-        {items.map(item => ( <h1>{item.name}</h1>))}
+        {items.map(item => (<CharacterItem key={item.char_id} item={item}/>))}
     </section>)
 }
 
