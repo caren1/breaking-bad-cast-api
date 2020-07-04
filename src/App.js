270708e9ff3a2ee2a4325899706117f4bdf9e1fe
caren1 <czarnocki.w@wp.pl>
2020-07-04 15:19:21 +0200

    initialized states, useState and useEffect and initial request with axios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/UI/Header'
+import axios from 'axios'
 
 const App = () => {
+
+  const [ items, setItems ] = useState([])
+  const [ isLoading, setIsLoading ] = useState(true)
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(`https://www.breakingbadapi.com/api/characters`)
+      console.log(result.data);
+    }
+    fetchItems()
+  }, [])
+
   return <div>
     <Header />
   </div>
